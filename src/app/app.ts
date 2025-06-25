137c0e49a65cb923e7a33453c0fce61e762d533f
Gabriel Pereira <gabrielpsilva023@gmail.com>
2025-06-25 01:32:16 -0300

    feat: WIP - Implementando home e login

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrimeNG } from 'primeng/config';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
+export class App implements OnInit {
+  private readonly primeng = inject(PrimeNG);
   protected title = 'stock-control';
+
+  ngOnInit(): void {
+    this.primeng.ripple.set(true);
+  }
 }
