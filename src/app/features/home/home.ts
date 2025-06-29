83732e1450d97e5099ad0b12096659db97d7a31b
Gabriel Pereira <gabrielpsilva023@gmail.com>
2025-06-29 15:13:29 -0300

    feat: Adicionando Reactive forms no Login e Signup

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
@@ -6,17 +6,48 @@ import { ButtonModule } from 'primeng/button';
 import { ToastModule } from 'primeng/toast';
 import { RippleModule } from 'primeng/ripple';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, CardModule, InputTextModule, ButtonModule, ToastModule, RippleModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    CardModule,
+    InputTextModule,
+    ButtonModule,
+    ToastModule,
+    RippleModule,
+  ],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
-  showLoginCard = true;
+  //Injects
+  private readonly formBuilder = inject(FormBuilder);
 
+  //Properties
+  showLoginCard = true;
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required, Validators.email],
+    password: ['', Validators.required],
+  });
+  signupForm = this.formBuilder.group({
+    name: ['', Validators.required, Validators.minLength(8)],
+    email: ['', Validators.required, Validators.email],
+    password: ['', Validators.required],
+  });
+
+  //Methods
   alternateShowLoginCard() {
     this.showLoginCard = !this.showLoginCard;
+  }
+
+  onSubmitLoginForm(): void {
+    console.log(this.loginForm.value);
+  }
+
+  onSubmitSignupForm(): void {
+    console.log(this.signupForm.value);
   }
 }
